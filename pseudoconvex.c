e37db81a4ac79d44ac412f01d43ce35ce33699da
nvcleemp <nico.vancleemput@gmail.com>
2009-05-20 12:30:01 +0000

    updated some function names and signatures

--- pseudoconvex.c
+++ pseudoconvex.c
@@ -9,11 +9,11 @@
 #include "pseudoconvex.h"
 
 /*========== EXPORT ===========*/
-void exportPlanarGraphCode(EDGE *start, int maxVertex){
+void exportPlanarGraphCode(){
 
 }
 
-void exportPlanarGraphTable(EDGE *start, int maxVertex){
+void exportPlanarGraphTable(){
 
 }
 
@@ -183,7 +183,7 @@ void fillPatch_3PentagonsLeft(int k1, int k2, int k3, INNERSPIRAL *is){
 void fillPatch_2PentagonsLeft(int k1, int k2, int k3, int k4, INNERSPIRAL *is){
 }
 
-void fillPatch_1PentagonsLeft(int k1, int k2, int k3, int k4, int k5, INNERSPIRAL *is){
+void fillPatch_1PentagonLeft(int k1, int k2, int k3, int k4, int k5, INNERSPIRAL *is){
 }
 
 void fillPatch_0PentagonsLeft(int k1, int k2, int k3, int k4, int k5, int k6, INNERSPIRAL *is){
--- pseudoconvex.h
+++ pseudoconvex.h
@@ -9,6 +9,7 @@
 /*========== DATA STRUCTURES ===========*/
 
 struct _innerspiral {
+	int length;     /* the length of the code (i.e. the number of pentagons in the patch) */
 	int position;   /* the current possition in the spiral code */
 	int *code;      /* the array containing the actual code */
 
@@ -17,8 +18,8 @@ struct _innerspiral {
 typedef struct _innerspiral INNERSPIRAL;
 
 /*========== EXPORT ===========*/
-void exportPlanarGraphCode(EDGE *start, int maxVertex);
-void exportPlanarGraphTable(EDGE *start, int maxVertex);
+void exportPlanarGraphCode();
+void exportPlanarGraphTable();
 
 
 /*========== CONSTRUCTION ==========*/
@@ -26,5 +27,5 @@ void fillPatch_5PentagonsLeft(int k, INNERSPIRAL *is);
 void fillPatch_4PentagonsLeft(int k1, int k2, INNERSPIRAL *is);
 void fillPatch_3PentagonsLeft(int k1, int k2, int k3, INNERSPIRAL *is);
 void fillPatch_2PentagonsLeft(int k1, int k2, int k3, int k4, INNERSPIRAL *is);
-void fillPatch_1PentagonsLeft(int k1, int k2, int k3, int k4, int k5, INNERSPIRAL *is);
+void fillPatch_1PentagonLeft(int k1, int k2, int k3, int k4, int k5, INNERSPIRAL *is);
 void fillPatch_0PentagonsLeft(int k1, int k2, int k3, int k4, int k5, int k6, INNERSPIRAL *is);
