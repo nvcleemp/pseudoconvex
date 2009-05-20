5fccba65ba81c40dd910ad3efea08f064c8987af
nvcleemp <nico.vancleemput@gmail.com>
2009-05-20 12:08:14 +0000

    fill method for patch with 3 pentagons

--- pseudoconvex.c
+++ pseudoconvex.c
@@ -75,7 +75,7 @@ void fillPatch_4PentagonsLeft(int k1, int k2, INNERSPIRAL *is){
 	} else {
 		//add a side of hexagons
 		is->code[is->position]+=k1+1;
-		fillPatch_4PentagonsLeft(k2-2, k1 + 1, is);
+		fillPatch_4PentagonsLeft(k2-2, k1+1, is);
 		is->code[is->position]-=k1+1;
 		
 		//pentagon after i hexagons
@@ -100,6 +100,84 @@ void fillPatch_4PentagonsLeft(int k1, int k2, INNERSPIRAL *is){
 }
 
 void fillPatch_3PentagonsLeft(int k1, int k2, int k3, INNERSPIRAL *is){
+	if(k1 < 0 || k2 < 0 || k3 < 0)
+		return;
+	int zeroes = 0;
+	if(k1==0) zeroes++;
+	if(k2==0) zeroes++;
+	if(k3==0) zeroes++;
+	if(zeroes>1) return;
+	
+	if(k1==0){
+		//only possibility: place a hexagon
+		is->code[is->position]+=1;
+		fillPatch_3PentagonsLeft(k2-1, k3-1, 1, is);
+		is->code[is->position]-=1;
+	} else if(k2==0){
+		//add a side of hexagons
+		is->code[is->position]+=k1+1;
+		fillPatch_3PentagonsLeft(0, k3-2, k1, is);
+		is->code[is->position]-=k1+1;
+		
+		//pentagon after i hexagons
+		int i;
+		for(i=0; i<k1-1; i++){
+			is->code[is->position]+=i;
+			is->position++;
+			is->code[is->position]=0;
+			fillPatch_2PentagonsLeft(k1-1-i, k2, k3-1, 1+i, is);
+			is->position--;
+			is->code[is->position]-=i;
+		}
+	} else if(k3==0){
+		//add a side of hexagons
+		is->code[is->position]+=k1+1;
+		fillPatch_3PentagonsLeft(k2-2, k3, k1, is);
+		is->code[is->position]-=k1+1;
+	
+		//pentagon after i hexagons
+		int i;
+		for(i=1; i<k1; i++){
+			is->code[is->position]+=i;
+			is->position++;
+			is->code[is->position]=0;
+			fillPatch_2PentagonsLeft(k1-1-i, k2-1, k3, i, is);
+			is->position--;
+			is->code[is->position]-=i;
+		}
+		
+		//pentagon after k1 hexagons
+		is->code[is->position]+=k1;
+		is->position++;
+		is->code[is->position]=0;
+		fillPatch_2PentagonsLeft(0, k2-2, k3, k1-1, is);
+		is->position--;
+		is->code[is->position]-=k1;
+	} else {
+		//add a side of hexagons
+		is->code[is->position]+=k1+1;
+		fillPatch_3PentagonsLeft(k2-1, k3-1, k1+1, is);
+		is->code[is->position]-=k1+1;
+		
+		//pentagon after i hexagons
+		int i;
+		for(i=0; i<k1; i++){
+			is->code[is->position]+=i;
+			is->position++;
+			is->code[is->position]=0;
+			fillPatch_2PentagonsLeft(k1-1-i, k2, k3-1, 1+i, is);
+			is->position--;
+			is->code[is->position]-=i;
+		}
+		
+		//pentagon after k1 hexagons
+		is->code[is->position]+=k1;
+		is->position++;
+		is->code[is->position]=0;
+		fillPatch_2PentagonsLeft(0, k2-1, k3-1, k1, is);
+		is->position--;
+		is->code[is->position]-=k1;
+	}
 }
 
 void fillPatch_2PentagonsLeft(int k1, int k2, int k3, int k4, INNERSPIRAL *is){
